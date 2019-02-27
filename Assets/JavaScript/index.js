9a68f7e28008b18bc6b6b60951d61e1a0289de3b
estherestherv <32546768+estherestherv@users.noreply.github.com>
2019-02-27 15:40:36 -0500

    saving changes

---
+++ Assets/JavaScript/index.js
@@ -0,0 +1,15 @@
+const products =[<img src="Assets/Images/tree.jpg"></img>];
+
+for(let i=0; i<products.length; i++){
+console.log(products[i]);
+}
+
+function products(){
+  
+}
+function removeProduct(){
+
+}
+function addProduct(){
+
+}
