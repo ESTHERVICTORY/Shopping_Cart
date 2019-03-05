90de0dd894bf9fec4ba3bc6d98ed20df70e8c6e7
estherestherv <32546768+estherestherv@users.noreply.github.com>
2019-03-05 11:40:16 -0500

    fgh

--- Assets/JavaScript/index.js
+++ Assets/JavaScript/index.js
@@ -1,108 +1,49 @@
+$(function(){
 
-const cart = [
-    {
-        id :1,
-        name: "panda",
-        price:"$2.98",
-        count:1,
-        image:'<img src="Assets/Images/Heart_Print.jpg"></img>'
-    }
-];
-
-var products = function (id,name, price, count) {
-    var $target = $(this);
-
-    var id = $target.data('id');
-    var name = $target.data('name');
-    var price= $target.image('price');
-    var count = $target.data('count');
-   
-    cart.push(id,cart.name,cart.price,cart.count);
-    var name =document.getElementsByClassName("name");
-console.log(name);
-}
-
-//    // $(document).on('click', targetSelector,function removeProduct(name ){  
-       
-//         for (let i=0; i<cart.length; i++) { 
-//             if (cart[i].name == name) {
-//                 cart[i].count--;
-//                 if (cart[i]===0){
-//                     cart.splice(i,1);
-//                 }
-//                 removeProduct();
-//                 break;
-//             }
-//         }   
-//     })
-// function removeProduct(name) {
-//     for (let i=0; i<cart.length; i++) { 
-//         if (cart[i].name == name) {
-//             cart[i].count--;
-//             if (cart[i]===0){
-//                 cart.splice(i,1);
-//             }
-//             break;
-//         }
-//     }
-// }
-//removeProduct();
-
-$(".add").on('click',function(){
-    var $target = $(this);
-
-    var id = $target.data('id');
-    var name = $target.data('name');
-    var price= $target.data('price');
-    var count = $target.data('count');
-   
-    cart.push(id,name,price,count);
-    $.each(cart, function (key, value){
-        alert(JSON.stringify(key+":"+value));
+    $("#cart-items").slideUp();
+    $(".cart").on("click", function () {
+    $("#cart-items").slideToggle();
     });
-}
 
-);
+    $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
 
+    
+    $(".item").on("click", function () {
+  $("#cart-items").slideDown();
+ setTimeout(function(){
+    $("#cart-items").slideUp();
+ }, 2000)
+        //add items to basket
+        $(this).each(function () {
+            var name = $(this).children(".item-detail").children("h4").text();
+            var remove = "<button class='remove'> X </button>";
+            var cena = "<span class='eachPrice'>" + (parseFloat($(this).children(".item-detail").children(".prices").children(".price").text())) + "</span>";
+            $("#list-item").append("<li>" + name + "&#09; - &#09;" + cena + "$" + remove + "</li>");
 
-//$(".add").on('click', targetSelector, addProduct(name, price, count, image) {
-    //console.log(name);
-    //for (let i=0; i<cart.length; i++) {
-       // if (cart[i].name == name) {
-          //  alert(cart[i] + "is already tarin cart");
-            
-        //}
-      //  console.log(cart[i]);
-    //}
-    //var product = newProduct(name, price, count, image);
-  //  cart.push(products);
-//}
-//)
-// addProduct();
+            //number of items in basket
+            $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+            $("#items-basket").text();
+    
+        //calculate total price
+        var totalPrice = 0;
+            $(".eachPrice").each(function (){ 
+              var cenaEach = parseFloat($(this).text());
+              totalPrice+=cenaEach;
+            });
+            $("#total-price").text(totalPrice + "$");
+        });
 
-$('#newStuff').html(`<div>${cart[name]}</div>`);
-function clearCart() {
-    $(".btn btn-primary").click(function(){  
-        cart.empty
-    })
-}
-clearCart();
+        //remove items from basket
+        $(".remove").on("click", function () {
+            $(this).parent().remove();
 
-    $("#toys").click(function(event){
-        $(".product").hide();
-        $(".toys").show();   
-    }) 
-
-    $("#product").click(function(){
-        $(".product").show();
-        $(".toys").hide();   
-    }) 
-
-    $("#home").click(function(){
-        $(".product").show();
-        $(".toys").show();   
-    }) 
-$('newStuff').html(`<h1>${cart}</h1>`);
-
-
-
+            var totalPrice = 0;
+            $(".eachPrice").each(function (){ 
+              var cenaEach = parseFloat($(this).text());
+              totalPrice+=cenaEach;
+            });
+            $("#total-price").text(totalPrice + "$");
+            $("#items-basket").text("(" + ($("#list-item").children().length) + ")");
+        });
+    });
+})
