bfd9b16c3c3ba864f41ef6dd7904fb5fe5ac3659
estherestherv <32546768+estherestherv@users.noreply.github.com>
2019-03-05 00:54:06 -0500

    bnm

--- Assets/JavaScript/index.js
+++ Assets/JavaScript/index.js
@@ -1,6 +1,7 @@
 
 const cart = [
     {
+        id :1,
         name: "panda",
         price:"$2.98",
         count:1,
@@ -8,39 +9,78 @@ const cart = [
     }
 ];
 
-var products = function (name, price, count, image) {
-    this.name = name
-    this.price = price
-    this.count = count
-    this.image = image
+var products = function (id,name, price, count) {
+    var $target = $(this);
+
+    var id = $target.data('id');
+    var name = $target.data('name');
+    var price= $target.image('price');
+    var count = $target.data('count');
+   
+    cart.push(id,cart.name,cart.price,cart.count);
+    var name =document.getElementsByClassName("name");
+console.log(name);
 }
 
+//    // $(document).on('click', targetSelector,function removeProduct(name ){  
+       
+//         for (let i=0; i<cart.length; i++) { 
+//             if (cart[i].name == name) {
+//                 cart[i].count--;
+//                 if (cart[i]===0){
+//                     cart.splice(i,1);
+//                 }
+//                 removeProduct();
+//                 break;
+//             }
+//         }   
+//     })
+// function removeProduct(name) {
+//     for (let i=0; i<cart.length; i++) { 
+//         if (cart[i].name == name) {
+//             cart[i].count--;
+//             if (cart[i]===0){
+//                 cart.splice(i,1);
+//             }
+//             break;
+//         }
+//     }
+// }
+//removeProduct();
 
-function removeProduct(name) {
-    for (let i=0; i<cart.length; i++) { //take a look at syntax for "for loops"
-        if (cart[i].name == name) {
-            cart[i].count--;
-            if (cart[i]===0){
-                cart.splice(i,1);
-            }
-            break;
-        }
-    }
+$(".add").on('click',function(){
+    var $target = $(this);
+
+    var id = $target.data('id');
+    var name = $target.data('name');
+    var price= $target.data('price');
+    var count = $target.data('count');
+   
+    cart.push(id,name,price,count);
+    $.each(cart, function (key, value){
+        alert(JSON.stringify(key+":"+value));
+    });
 }
-removeProduct();
 
-function addProduct(name, price, count, image) {
-    for (let i=0; i<cart.length; i++) {
-        if (cart[i].name == name) {
-            alert(cart[i] + "is already in cart");
-            break;
-        }
-    }
-    var product = newProduct(name, price, count, image);
-    cart.push(newProduct);
-}
-addProduct();
+);
 
+
+//$(".add").on('click', targetSelector, addProduct(name, price, count, image) {
+    //console.log(name);
+    //for (let i=0; i<cart.length; i++) {
+       // if (cart[i].name == name) {
+          //  alert(cart[i] + "is already tarin cart");
+            
+        //}
+      //  console.log(cart[i]);
+    //}
+    //var product = newProduct(name, price, count, image);
+  //  cart.push(products);
+//}
+//)
+// addProduct();
+
+$('#newStuff').html(`<div>${cart[name]}</div>`);
 function clearCart() {
     $(".btn btn-primary").click(function(){  
         cart.empty
@@ -48,7 +88,7 @@ function clearCart() {
 }
 clearCart();
 
-    $("#toys").click(function(){
+    $("#toys").click(function(event){
         $(".product").hide();
         $(".toys").show();   
     }) 
@@ -63,5 +103,6 @@ clearCart();
         $(".toys").show();   
     }) 
 $('newStuff').html(`<h1>${cart}</h1>`);
-console.log(cart[i]);
+
+
 
